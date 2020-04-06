ff0d02c7db8d4c610f85f3d0c9eaa4755f6cea73
xiaolin <462441355@qq.com>
2020-04-06 19:14:44 +0800

    * 样式测试

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss'
 
-ReactDOM.render(<div>你好</div>, document.getElementById('root'));
+ReactDOM.render(<div className="name">你好</div>, document.getElementById('root'));
