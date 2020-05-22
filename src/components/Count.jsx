a0fc5ae7adb2f15ede0786378a8bde5238957a6e
xiaolin <462441355@qq.com>
2020-05-22 22:45:52 +0800

    * React.PureComponent

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -2,59 +2,105 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 /**
- * 创建一个上下文对象
- *  ThemeContext.provider
+ * React.PureComponent
  */
-let ThemeContext = React.createContext()
-
-class CountButton extends React.Component {
-    static contextType = ThemeContext
-
-    render() {
-        return <ThemeContext.Consumer>
-            {context => {
-                return <button onClick={ev => {
-                    context.setNum()
-                }}>累加</button>
-            }}
-        </ThemeContext.Consumer>
+// 比较两个对象 如果属性值是基本类型的，我们只需要比较是否一样
+function shallowEqual(obj1, obj2) {
+    let flag = true
+    if (Object.keys(obj1).length !== Object.keys(obj2).length) return false
+    for (let key in obj1) {
+        if (obj1[key] !== obj2[key]) {
+            return false
+        }
     }
-}
 
+    return flag
+}
 class CountShow extends React.Component {
-    static contextType = ThemeContext
-    
 
     render() {
         return <div>
-            {this.context.num}
+            {this.props.num}
         </div>
     }
 }
 
-export default class Count extends React.Component {
+export default class Count extends React.PureComponent {
  
     state = {
         num: 0
     }   
 
     render() {
-        return <ThemeContext.Provider value={{
-            num: this.state.num,
-            setNum: this.setNum
-        }}>
-            <h3>计数器</h3>
-            <CountShow />
-            <CountButton />
-        </ThemeContext.Provider>
+        return <div>
+            <CountShow num={this.state.num} />
+            <button onClick={ev => {
+                this.setState({
+                    num: this.state.num + 1
+                })
+            }}>累加</button>
+        </div>
     }
 
-    setNum = (num) => {
-        this.setState({
-            num: this.state.num + 1
-        })
-    }
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     return !shallowEqual(this.state, nextState) || !shallowEqual(this.props, nextProps)
+    // }
 }
+
+/**
+ * 创建一个上下文对象
+ *  ThemeContext.provider
+ */
+// let ThemeContext = React.createContext()
+
+// class CountButton extends React.Component {
+//     static contextType = ThemeContext
+
+//     render() {
+//         return <ThemeContext.Consumer>
+//             {context => {
+//                 return <button onClick={ev => {
+//                     context.setNum()
+//                 }}>累加</button>
+//             }}
+//         </ThemeContext.Consumer>
+//     }
+// }
+
+// class CountShow extends React.Component {
+//     static contextType = ThemeContext
+    
+
+//     render() {
+//         return <div>
+//             {this.context.num}
+//         </div>
+//     }
+// }
+
+// export default class Count extends React.Component {
+ 
+//     state = {
+//         num: 0
+//     }   
+
+//     render() {
+//         return <ThemeContext.Provider value={{
+//             num: this.state.num,
+//             setNum: this.setNum
+//         }}>
+//             <h3>计数器</h3>
+//             <CountShow />
+//             <CountButton />
+//         </ThemeContext.Provider>
+//     }
+
+//     setNum = (num) => {
+//         this.setState({
+//             num: this.state.num + 1
+//         })
+//     }
+// }
 
 // class CountButton extends React.Component {
 //     static contextTypes = {
