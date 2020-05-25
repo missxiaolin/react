a886710ce1a7d9547a7a0dc84dd44e79187bf162
xiaolin <462441355@qq.com>
2020-05-25 21:58:42 +0800

    * redux 测试

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './assets/index.scss'
 import Vote from './components/reactComponent'
 
+
 ReactDOM.render(<div className="name">
     <Vote />
 </div>, document.getElementById('root'));
