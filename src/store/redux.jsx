e086eb1b4224ce369a83122bed13e2ad91df84c0
xiaolin <462441355@qq.com>
2020-05-30 16:02:51 +0800

    * 实现redux combineReducers

--- src/store/redux.jsx
+++ src/store/redux.jsx
@@ -2,6 +2,8 @@
  * createStore(reducer)创建容器，并且基于reducer可以管理容器中的状态
  */
 
+import { func } from "prop-types"
+
 export function createStore(reducer) {
     // 构建状态池和事件池
     let state,
@@ -39,5 +41,20 @@ export function createStore(reducer) {
         getState,
         subscribe,
         dispatch
+    }
+}
+
+/**
+ * combineReducers 合并reducer
+ */
+export function combineReducers(reducers) {
+    return function reducer(state = {}, action) {
+        // state 容器重的总状态{computed: {}, vote: {}}
+        // 每一次派发执行总得reducer，都会让每一个模块下的reducer执行
+        for (let key in reducers) {
+            if (!reducers.hasOwnProperty(key)) break
+            state[key] = reducers[key](state[key], action)
+        }
+        return state
     }
 }
