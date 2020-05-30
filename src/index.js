2225028df759b5bfabd506d32d95d1528f4ecc19
xiaolin <462441355@qq.com>
2020-05-30 20:59:31 +0800

    * 引入antd

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/index.scss'
-// import Vote from './components/reactComponent'
 import store from './store/index'
 import { Provider } from 'react-redux'
-import Text from './components/Text'
 
 ReactDOM.render(<Provider store={store}>
-    <Text />
 </Provider>, document.getElementById('root'));
