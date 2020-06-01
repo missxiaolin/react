e346269f81142d038776999ab6d5f8addefc35f0
xiaolin <462441355@qq.com>
2020-06-02 07:48:34 +0800

    * add

--- src/view/Nav.jsx
+++ src/view/Nav.jsx
@@ -6,12 +6,11 @@ class Nav extends Component {
         console.log(this.props)
         return <div>
             <header>
-                <a onClick={ev => {
-                    console.log(this.props)
-                    // this.props.history.push('/cs')
+                <a href="/cs" onClick={ev => {
+                    this.props.history.push('/cs')
                 }}>测试链接1</a>
-                <a onClick={ev => {
-                    // this.props.history.push('/')
+                <a href="/" onClick={ev => {
+                    this.props.history.push('/')
                 }}>测试链接2</a>
             </header>
         </div>
