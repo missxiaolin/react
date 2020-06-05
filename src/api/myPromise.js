e6f8e2492c548934b15ad6a7c90ab1a5d783fa20
xiaolin <462441355@qq.com>
2020-06-05 23:53:51 +0800

    * 初始化

--- src/api/myPromise.js
+++ src/api/myPromise.js
@@ -1,3 +1,21 @@
 /**
  * Promise A+: Promise的设计规范（基于原生js自己可以实现一套内置Promise具备的功能）
  */
+
+ class MyPromise {
+     constructor(executor) {
+         // 初始参数
+        this.status = 'pending'
+        this.value = undefined
+        
+        let resolve = function (result) {
+
+        }
+        let reject = function(reason) {
+
+        }
+        executor(resolve, reject)
+     }
+}
+
+module.exports = MyPromise
